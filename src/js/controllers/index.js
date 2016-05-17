2766b6ca23123f68c864bb101dbc9507f75d936f
Brian Reilly <rilesdbrian@gmail.com>
2016-05-17 13:57:26 -0400

    Added graph tooltips

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -1,3 +1,4 @@
 var app = angular.module('syntaxDash', []);
 
-app.controller('syntaxDashCtrl', require('./dashboard.js'));
+app.controller('syntaxDashCtrl', require('./dashboard.js'))
+  .directive('svgTooltip', require('../directives/tooltip.js'));
---
+++ src/js/directives/tooltip.js
@@ -0,0 +1,49 @@
+module.exports = function () {
+  'use strict';
+
+  var tooltip = {
+    el: document.querySelectorAll('.svg-tooltip')[0],
+    hide: function () {
+      var tooltipStyle = this.el.style;
+      tooltipStyle.opacity = 0;
+      tooltipStyle.visibility = 'hidden';
+    },
+
+    show: function () {
+      var tooltipStyle = this.el.style;
+      tooltipStyle.opacity = 1;
+      tooltipStyle.visibility = 'visible';
+    },
+  };
+
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var rawEl = element[0];
+      var tooltipContent = attrs.svgTooltip;
+      var tooltipEl = tooltip.el;
+      var tooltipStyle = tooltipEl.style;
+
+      element.on('mouseover', function () {
+        tooltipEl.innerHTML = tooltipContent;
+        tooltipStyle.display = 'block';
+        tooltip.hide();
+        var tooltipHeight = tooltipEl.offsetHeight;
+        var tooltipWidth = tooltipEl.offsetWidth;
+        var boundElDim = rawEl.getBoundingClientRect();
+
+        var top = boundElDim.top - tooltipHeight - 5;
+        var left = boundElDim.left + boundElDim.width / 2 - tooltipWidth / 2;
+
+        tooltipStyle.left = left + 'px';
+        tooltipStyle.top = top + 'px';
+        tooltip.show();
+      });
+
+      element.on('mouseout', function () {
+        tooltip.hide();
+        tooltipEl.style.display = 'none';
+      });
+    },
+  };
+};
