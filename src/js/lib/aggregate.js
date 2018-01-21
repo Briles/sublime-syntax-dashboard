f1f1ea72da7e2ed1a8f5d006d021735431a69570
Brian Reilly <Briles@users.noreply.github.com>
2018-01-20 20:10:38 -0500

    updated data

--- src/js/lib/aggregate.js
+++ src/js/lib/aggregate.js
@@ -1,1 +1,1 @@
-module.exports={"scopes":144,"unique scopes":57,"includes":66,"unique includes":16,"patterns":98,"unique patterns":45,"avg. specificity":4};
+module.exports={"scopes":144,"unique scopes":58,"includes":66,"unique includes":16,"patterns":98,"unique patterns":45,"avg. specificity":4};
